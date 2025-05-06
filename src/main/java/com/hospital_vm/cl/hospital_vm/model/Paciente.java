0926d3b723607a13b1404cd35f8343d5b7491245
Renato Hinojosa <Ren.hinojosa@duocuc.cl>
2025-05-06 14:12:34 -0400

    Inicio Proyecto

---
+++ src/main/java/com/hospital_vm/cl/hospital_vm/model/Paciente.java
@@ -0,0 +1,28 @@
+package com.hospital_vm.cl.hospital_vm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="paciente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true, length = 13,nullable = false)
+    private String run;
+    @Column(nullable = false)
+    private String nombres;
+    @Column(nullable = false)
+    private String apellidos;
+    @Column(nullable = true)
+    private String fechaNacimiento;
+    @Column(nullable = false)
+    private String correo;
+}
